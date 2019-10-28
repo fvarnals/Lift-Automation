b70051a359bc7633636969b20ddbde4ace021d6b
Freddie Varnals <f.varnals@gmail.com>
2019-10-28 23:43:23 +0000

    Lift_controller.people_in_lift returns False if no one is in the lift

--- tests/lift_controller_test.py
+++ tests/lift_controller_test.py
@@ -8,9 +8,12 @@ from lift import Lift
 
 queues = ( (),   (),    (), (),   (3,),    (),    () )
 capacity = 5
+lift = Lift(queues,capacity)
+lift_controller = LiftController(lift)
 
+def test_people_queueing():
+    assert lift_controller.people_queueing == True
 
-def test_lift_active():
-        lift = Lift(queues,capacity)
-        lift_controller = LiftController(lift)
-        assert lift_controller.people_queueing == True
+def test_people_in_lift():
+    assert lift_controller.people_in_lift == False
+    
